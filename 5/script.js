1b28170b41b3ab7f1af68c51ccb1554537bab976
OT4EHb <guda1930@gmail.com>
2024-10-08 18:02:37 +0300

    Valid js

--- 5/script.js
+++ 5/script.js
@@ -3,10 +3,9 @@
     let c = document.getElementById("count");
     let p = document.getElementById("price");
     let r = document.getElementById("cost");
-    if (c.value.match(/^\d+$/) == null) {
+    if (c.value.match(/^\d+$/) === null) {
         r.innerHTML = "Некорректно";
-    }
-    else {
+    } else {
         r.innerHTML = "Итоговая стоимость: " + (c.value * p.textContent.match(/\d+/)) + " руб";
     }
 }
@@ -18,35 +17,33 @@ function selectFunction() {
     let img = document.getElementById("img");
     r.textContent = "";
     p.textContent = "Текущая цена: ";
-    img.src = "../"
+    img.src = "../";
     switch (s.value) {
-        case "v1":
-            p.textContent += 2999;
-            img.src += "image.jpg";
-            break;
-        case "v2":
-            p.textContent += 379;
-            img.src += "pelmeni.jpg";
-            break;
-        case "v3":
-            p.textContent += 45;
-            img.src += "doshirak.png"
-            break;
-        case "v4":
-            p.textContent += 88;
-            img.src += "makaroshki.png";
-            break;
-        case "v5":
-            p.textContent += 1199;
-            img.src += "Final.jpg";
-            break;
-        default:
-            break;
+    case "v1":
+        p.textContent += 2999;
+        img.src += "image.jpg";
+        break;
+    case "v2":
+        p.textContent += 379;
+        img.src += "pelmeni.jpg";
+        break;
+    case "v3":
+        p.textContent += 45;
+        img.src += "doshirak.png";
+        break;
+    case "v4":
+        p.textContent += 88;
+        img.src += "makaroshki.png";
+        break;
+    case "v5":
+        p.textContent += 1199;
+        img.src += "Final.jpg";
+        break;
     }
     p.textContent += " руб";
 }
-window.addEventListener("DOMContentLoaded", function (event) {
+window.addEventListener("DOMContentLoaded", function () {
     document.getElementById("sel").addEventListener("change", selectFunction);
     document.getElementById("but").addEventListener("click", click1);
     selectFunction();
-})
+});
