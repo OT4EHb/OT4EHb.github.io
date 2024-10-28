36d699590101e1ddcb828a9f91ac0a3815763315
OT4EHb <guda1930@gmail.com>
2024-10-28 09:16:36 +0300

    Added historyBack

--- 8/script.js
+++ 8/script.js
@@ -6,6 +6,12 @@
 
 }
 
+function historyBack() {
+    window.history.back();
+    return false;
+}
+
 window.addEventListener("DOMContentLoaded", function () {
     document.getElementById("buttonModal").addEventListener("click", historyAdd);
+    document.getElementById("buttonClose").addEventListener("click", historyBack);
 });
