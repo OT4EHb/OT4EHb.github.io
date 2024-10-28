642af454e9174e56d98c573dbf4513698b2e51bf
OT4EHb <guda1930@gmail.com>
2024-10-28 09:05:39 +0300

    Add files via upload

---
+++ 8/script.js
@@ -0,0 +1,11 @@
+﻿function historyAdd() {
+    let stateObj = {
+        foo: "bar",
+    };
+    window.history.pushState(stateObj, "page 2", "1.html");
+
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("buttonModal").addEventListener("click", historyAdd);
+});
