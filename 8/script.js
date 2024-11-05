b369d6b0f02263d44e4a614f1e68f5b9a3a3ece7
OT4EHb <guda1930@gmail.com>
2024-11-05 20:35:47 +0300

    Complete lab8

--- 8/script.js
+++ 8/script.js
@@ -1,17 +1,44 @@
 ﻿function historyAdd() {
     let stateObj = {
-        foo: "bar",
+        modal: "true"
     };
-    window.history.pushState(stateObj, "page 2", "1.html");
-
+    window.history.pushState(stateObj, "", "#exampleModal");
 }
 
 function historyBack() {
     window.history.back();
-    return false;
+}
+
+function historyChange() {
+    const elemModal = document.querySelector("#exampleModal");
+    if (window.location.href.match(/#/) !== null) {
+        new bootstrap.Modal(elemModal).show();
+    } else {
+        bootstrap.Modal.getInstance(elemModal).hide();
+    }
+}
+
+function saveValue() {
+    window.localStorage.setItem(this.name, this.value);
+}
+
+function saveForm() {
+    window.localStorage.clear();
 }
 
 window.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("buttonModal").addEventListener("click", historyAdd);
-    document.getElementById("buttonClose").addEventListener("click", historyBack);
+    let butM = document.getElementById("buttonModal");
+    butM.addEventListener("click", historyAdd);
+    let butC = document.getElementById("buttonClose");
+    butC.addEventListener("click", historyBack);
+    window.addEventListener("popstate", historyChange);
+    let inputs = document.querySelectorAll(".form-control:not(.form-label)");
+    inputs.forEach(function (i) {
+        i.addEventListener("input", saveValue);
+    });
+    let lStor = window.localStorage;
+    Object.keys(lStor).forEach(function (i) {
+        document.getElementsByName(i)[0].value = lStor.getItem(i);
+    });
+    document.getElementById("buttonSave").addEventListener("click", saveForm);
 });
